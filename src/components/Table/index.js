fb6674ed9f750e7d8a8326d611166ae13a3a7eb7
Vladimir <komarov2142@gmail.com>
2020-02-06 14:41:41 +1000

    mock api

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -22,7 +22,7 @@ const mapDispatchToProps = dispatch => {
 function Table({ data, dataLoaded, dataLoadingFailed, getData }) {
   useEffect(() => {
     if (!dataLoadingFailed && !dataLoaded) {
-      getData("http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}");
+      getData("https://5e3ae184f2cb300014390d21.mockapi.io/users");
     }
   });
 
